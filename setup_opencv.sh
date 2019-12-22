beff48e710d3b159452e8b5ef7855c292b80d0f3
Areeb Jamal <jamal.areeb@gmail.com>
2019-12-22 19:14:04 +0530

    chore: Release 4.2.0-contrib (#12)

--- setup_opencv.sh
+++ setup_opencv.sh
@@ -4,14 +4,13 @@ set -e
 
 # Download OpenCV Android SDK
 
-opencv_version="4.1.1"
 opencv_sdk_zip="cache/opencv-android-sdk.zip"
 
 if [[ $1 = "--skip-download" ]]; then
     echo "Skipping Download of ${opencv_sdk_zip}..."
 else
     echo "Downloading ${opencv_sdk_zip}..."
-    download_url="https://pullrequest.opencv.org/buildbot/export/opencv_releases/master-contrib_pack-contrib-android/20191010-050206--10946/OpenCV4Android.zip"
+    download_url="https://pullrequest.opencv.org/buildbot/export/opencv_releases/master-contrib_pack-contrib-android/20191222-054115--11020/OpenCV4Android.zip"
     echo "Download URL: ${download_url}"
     wget -c -O ${opencv_sdk_zip} ${download_url}
 fi
